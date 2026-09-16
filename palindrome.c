0df53ebad4ad37de1a7754132beab4d66cb25cf6
Kali <danii.dispose@gmail.com>
2026-09-16 16:55:30 -0400

    did homework: armstrongno, primeno

--- palindrome.c
+++ palindrome.c
@@ -3,11 +3,17 @@ int main(){
   int n, r = 0;
   printf("Enter  number: ");
   scanf("%d", &n);
+
   while(n>0){
     r = r*10 + n%10;
     n = n/10;
   }
-  if()
-  printf("Reverse = %d \n", r);
+
+  if(n == r) {
+    printf("it is palindrome")
+  }
+  else {
+    printf("it is not palindrome")
+  }
   return 0;
 }
---
+++ primeno.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, i = 2, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (i < n) {
+        if (n % i == 0) {
+            count++;
+            break;
+        }
+        i++;
+    }
+
+    if (n > 1 && count == 0)
+        printf("Prime number");
+    else
+        printf("Not a prime number");
+
+    return 0;
+}
+
